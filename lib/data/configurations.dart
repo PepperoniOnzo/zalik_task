b1868701f97d99cf5bca69462b12eb0767c6f8bc
Pepperoni <radchukmaxym@gmail.com>
2022-11-16 20:23:27 +0200

    Init

---
+++ lib/data/configurations.dart
@@ -0,0 +1,61 @@
+class Configs {
+  static const String tagInstitute = 'INSTITUTE', tagGroup = 'GROUP';
+
+  static const String link =
+      "https://student.lpnu.ua/students_schedule?departmentparent_abbrname_selective=INSTITUTE&studygroup_abbrname_selective=GROUP&semestrduration=1";
+
+  static const Map<String, List<String>> shedule = {
+    'ІКТА': [
+      'КІ-11',
+      'КІ-110',
+      'КІ-12',
+      'КІ-13',
+      'КІ-14',
+      'КІ-15',
+      'КІ-16',
+      'КІ-17',
+      'КІ-18',
+      'КІ-19',
+      'КІ-201',
+      'КІ-202',
+      'КІ-203',
+      'КІ-204',
+      'КІ-205',
+      'КІ-206',
+      'КІ-207',
+      'КІ-208',
+      'КІ-209',
+      'КІ-210',
+      'КІ-211',
+      'КІ-31',
+      'КІ-32',
+      'КІ-33',
+      'КІ-34',
+      'КІ-35',
+      'КІ-36',
+      'КІ-37',
+      'КІ-38',
+      'КІ-39',
+      'КІ-41',
+      'КІ-42',
+      'КІ-43',
+      'КІ-44',
+      'КІ-45',
+      'КІ-46',
+      'КІ-47',
+      'КІ-48'
+    ],
+    'ІКНІ': [
+      'СА-11',
+      'СА-12',
+      'СА-21',
+      'СА-22',
+      'СА-23',
+      'СА-31',
+      'СА-32',
+      'СА-33',
+      'СА-41',
+      'СА-42'
+    ]
+  };
+}
